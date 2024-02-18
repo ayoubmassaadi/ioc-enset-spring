80d5b5c2234519db54aa7f7a183d8642c736f1a2
ayoub <ayoubmassaadi2019@gmail.com>
2024-02-18 13:05:57 +0100

    v1

---
+++ src/main/java/presentation/Presentation.java
@@ -0,0 +1,14 @@
+package presentation;
+
+import dao.DaoImpl;
+import metier.MetierImpl;
+
+public class Presentation {
+    public static void main(String[] args) {
+        DaoImpl dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+
+        metier.setDao(dao);
+        System.out.println("result : "+ metier.calcul());
+    }
+}
