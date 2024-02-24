b1945b4dd8cdf90ee5b38b93199631d33dbc23f3
ayoub <ayoubmassaadi2019@gmail.com>
2024-02-25 00:43:48 +0100

    final version

--- src/main/java/presentation/Presentation.java
+++ src/main/java/presentation/Presentation.java
@@ -6,9 +6,11 @@ import metier.MetierImpl;
 public class Presentation {
     public static void main(String[] args) {
         DaoImpl dao = new DaoImpl();
+        /*
         MetierImpl metier = new MetierImpl();
 
         metier.setDao(dao);
         System.out.println("result : "+ metier.calcul());
+        */
     }
 }
--- src/main/java/presentation/PresentationSpringAnnotation.java
+++ src/main/java/presentation/PresentationSpringAnnotation.java
@@ -1,10 +1,13 @@
 package presentation;
 
+import metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class PresentationSpringAnnotation {
     public static void main(String[] args) {
-        ApplicationContext contect = new AnnotationConfigApplicationContext("dao","metier");
+        ApplicationContext context = new AnnotationConfigApplicationContext("ext","metier","dao");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println("RES="+metier.calcul());
     }
 }
